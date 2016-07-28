03ba1102c8b7b1c0e466e6d97aeff643d71f48c9
Chris Zhao <xinyue.zhao@immonet.de>
2016-07-28 13:56:01 +0200

    Add "this month", some wordings

--- index.go
+++ index.go
@@ -8,19 +8,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"time"
 )
 
 const (
 	API_INDEX_START = 2
 	API_INDEX_END   = 6
 	ERR             = "Sick, sorry I have some internal problems. :(. Try again or later."
+	DONE            = ":) Done!"
+
 	//Buttons
-	BTN_PAYLOAD_YES = "Yes, show something."
-	BTN_PAYLOAD_NO  = "No, thanks"
+	BTN_PAYLOAD_YES      = "Yes, show last 3 days photos."
+	BTN_PAYLOAD_NO       = "No, thanks."
+	BTN_START_THIS_MONTH = "This month photos please."
+	BTN_STOP_THIS_MONTH  = "No more photos, thanks."
 
 	//Payloads
-	PAYLOAD_YES = "POSTBACK_BUTTON_YES"
-	PAYLOAD_NO  = "POSTBACK_BUTTON_NO"
+	PAYLOAD_YES              = "POSTBACK_BUTTON_YES"
+	PAYLOAD_NO               = "POSTBACK_BUTTON_NO"
+	PAYLOAD_START_THIS_MONTH = "POSTBACK_BUTTON_START_THIS_MONTH"
+	PAYLOAD_STOP_THIS_MONTH  = "POSTBACK_BUTTON_STOP_THIS_MONTH"
 )
 
 type Photos struct {
@@ -61,7 +68,8 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		msg := NewButtonTemplate("Welcome to use NasaPic, do you want to get last three days photos from NASA?")
 		yesBtn := NewPostbackButton(BTN_PAYLOAD_YES, PAYLOAD_YES)
 		NoBtn := NewPostbackButton(BTN_PAYLOAD_NO, PAYLOAD_NO)
-		msg.AddButton(yesBtn, NoBtn)
+		thisMonthBtn := NewPostbackButton(BTN_START_THIS_MONTH, PAYLOAD_START_THIS_MONTH)
+		msg.AddButton(yesBtn, thisMonthBtn, NoBtn)
 
 		msgBot.Send(user, msg, NotificationTypeRegular)
 		hasErrorCh <- false
@@ -82,17 +90,64 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 			hasErrorCh <- false
 		case PAYLOAD_YES:
 			for i := API_INDEX_START; i <= API_INDEX_END; i++ {
-				success, photos := getPhotos(cxt, msgBot.Client, i)
+				success, photos := get3DaysPhotos(cxt, msgBot.Client, i)
 				if success {
 					for _, res := range photos.Result {
 						msgBot.Send(user, NewMessage(fmt.Sprintf("Photo of %s", res.Date)), NotificationTypeRegular)
 						msgBot.Send(user, NewImageMessage(res.Urls.HD), NotificationTypeRegular)
 					}
+					msgBot.Send(user, NewMessage(DONE), NotificationTypeRegular)
+					//Asking for getting photos of this month.
+					now := time.Now()
+					year := now.Year()
+					month := now.Month()
+					msg := NewButtonTemplate(fmt.Sprintf("Get whole photo list this month (%s/%d)?", month, year))
+					yesBtn := NewPostbackButton(BTN_START_THIS_MONTH, PAYLOAD_START_THIS_MONTH)
+					NoBtn := NewPostbackButton(BTN_STOP_THIS_MONTH, PAYLOAD_STOP_THIS_MONTH)
+					msg.AddButton(yesBtn, NoBtn)
+
+					msgBot.Send(user, msg, NotificationTypeRegular)
 					hasErrorCh <- false
 					return
 				}
 			}
-			//Some error happened before, otherwise you can't arrive here.
+			//Some errors happened before, otherwise you can't arrive here.
+			msgBot.Send(user, NewMessage(ERR), NotificationTypeRegular)
+			hasErrorCh <- true
+		case PAYLOAD_STOP_THIS_MONTH:
+			msgBot.Send(user, NewMessage("Sad! But still happy to see you here. :) "), NotificationTypeRegular)
+			hasErrorCh <- true
+		case PAYLOAD_START_THIS_MONTH:
+			for i := API_INDEX_START; i <= API_INDEX_END; i++ {
+				success, photos := getMonthPhotos(cxt, msgBot.Client, i)
+				if success {
+					var pMsg *GenericTemplate
+					i := 0
+					for _, res := range photos.Result {
+						if i%5 == 0 { //Every 5 box to show.
+							if pMsg != nil {
+								cxt.Infof(fmt.Sprintf("Send:%v", pMsg))
+								msgBot.Send(user, *pMsg, NotificationTypeRegular)
+							}
+							msg := NewGenericTemplate()
+							pMsg = &msg
+						}
+						element := Element{
+							Title:    res.Title,
+							Url:      res.Urls.HD,
+							ImageUrl: res.Urls.HD,
+							Subtitle: res.Date,
+						}
+						pMsg.AddElement(element)
+						cxt.Infof(fmt.Sprintf("Add:%v", element))
+						i++
+					}
+					msgBot.Send(user, NewMessage(DONE), NotificationTypeRegular)
+					hasErrorCh <- false
+					return
+				}
+			}
+			//Some errors happened before, otherwise you can't arrive here.
 			msgBot.Send(user, NewMessage(ERR), NotificationTypeRegular)
 			hasErrorCh <- true
 		}
@@ -100,8 +155,42 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	msgBot.Handler(w, r)
 }
 
-func getPhotos(cxt appengine.Context, httpClient *http.Client, apiIndex int) (success bool, photos *Photos) {
+func get3DaysPhotos(cxt appengine.Context, httpClient *http.Client, apiIndex int) (success bool, photos *Photos) {
 	response, err := httpClient.Post(fmt.Sprintf("http://nasa-photo-dev%d.appspot.com/last_three_list", apiIndex), "application/json", bytes.NewBufferString(fmt.Sprintf(`{"reqId":"%s","timeZone":"CET"}`, NewV4().String())))
+	if err != nil {
+		success = false
+		photos = nil
+		cxt.Errorf(fmt.Sprintf("Error: %v", err))
+		return
+	}
+	if response != nil {
+		defer response.Body.Close()
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		success = false
+		photos = nil
+		cxt.Errorf(fmt.Sprintf("Error: %v", err))
+		return
+	}
+	if response.StatusCode == http.StatusOK {
+		success = true
+		photos = &Photos{}
+		json.Unmarshal(body, photos)
+	} else {
+		success = false
+		photos = nil
+		cxt.Errorf(fmt.Sprintf("Status: %v", response.StatusCode))
+	}
+	return
+}
+
+func getMonthPhotos(cxt appengine.Context, httpClient *http.Client, apiIndex int) (success bool, photos *Photos) {
+	now := time.Now()
+	year := now.Year()
+	month := int(now.Month())
+
+	response, err := httpClient.Post(fmt.Sprintf("http://nasa-photo-dev%d.appspot.com/month_list", apiIndex), "application/json", bytes.NewBufferString(fmt.Sprintf(`{"reqId":"%s","year":%d, "month" : %d, "timeZone":"CET"}`, NewV4().String(), year, month)))
 	if err != nil {
 		success = false
 		photos = nil
